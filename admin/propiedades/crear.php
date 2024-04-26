5bbbe3e478f141f235af0d632498ed42c4a34008
YailCastillo <yailcastillomtz@gmail.com>
2024-04-26 12:56:07 -0600

    crear in propiedades added

---
+++ admin/propiedades/crear.php
@@ -0,0 +1,13 @@
+<?php
+    require '../../includes/functions.php';
+    incluirTemplate('header2');
+    
+?>
+
+<main class="contenedor seccion">
+    <h1>Titulo Página</h1>
+</main>
+
+<?php
+    incluirTemplate('footer');
+?>
