68903a5b8a7d6e1a96f24bc404e19b9e14f292a9
YailCastillo <yailcastillomtz@gmail.com>
2024-04-30 13:54:00 -0600

    Link to db added

---
+++ includes/config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+function conectarDB() {
+    $db = mysqli_connect('localhost', 'root', 'perolin1', 'bienesraices_crud');
+
+    if (!$db) {
+        echo 'Error, no se pudo conectar';
+        exit;
+    }
+
+    return $db;
+}
