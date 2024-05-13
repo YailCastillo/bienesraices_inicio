c3e69ccd482699a0bd08d4415139f45d39836df0
YailCastillo <yailcastillomtz@gmail.com>
2024-05-13 13:36:02 -0600

    "html" extensions changed to "php"

--- views/site/index.php
+++ views/site/index.php
@@ -36,14 +36,14 @@
     ?>
 
     <div class="alinear-derecha">
-        <a href="anuncios.html" class="boton-verde">Ver todas</a>
+        <a href="anuncios.php" class="boton-verde">Ver todas</a>
     </div>
 </section>
 
 <section class="imagen-contacto seccion">
     <h2>Encuentra la casa de tus sueños</h2>
     <p>Llena el formulario de contacto y un asesor se pondrá en contacto contigo a la brevedad</p>
-    <a href="contacto.html" class="boton-amarillo">Contáctanos</a>
+    <a href="contacto.php" class="boton-amarillo">Contáctanos</a>
 </section>
 
 <div class="contenedor seccion seccion-inferior">
@@ -59,7 +59,7 @@
                 </picture>
             </div>
             <div class="texto-entrada">
-                <a href="entrada.html">
+                <a href="entrada.php">
                     <h4>Terraza en el techo de tu casa</h4>
                     <p class="informacion-meta">Escrito el: <span>15/04/2024</span> por: <span>Admin</span></p>
                     
@@ -76,7 +76,7 @@
                 </picture>
             </div>
             <div class="texto-entrada">
-                <a href="entrada.html">
+                <a href="entrada.php">
                     <h4>Guía para la decoración de tu hogar</h4>
                     <p class="informacion-meta">Escrito el: <span>15/04/2024</span> por: <span>Admin</span></p>
 
