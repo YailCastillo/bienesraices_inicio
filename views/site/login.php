49a2f830ef69d31b49f3f03af7ff7c46813710cd
YailCastillo <yailcastillomtz@gmail.com>
2024-05-15 12:33:23 -0600

    login added

--- views/site/login.php
+++ views/site/login.php
@@ -1,27 +1,81 @@
 <?php
+    //Conexión a la base de datos
+    require '../../includes/config/db.php';
+    $db = conectarDB();
+
+    //Autenticar usuario
+
+    $errores = [];
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+
+        if (!$email) {
+            $errores[] = "El email ingresado no es válido";
+        }
+        if (!$password) {
+            $errores[] = "La contraseña es obligatoria";
+        }
+        if (empty($errores)) {
+            //Revisar si usuario existe
+            $query = "SELECT * FROM usuarios WHERE email = '$email'";
+            $resultado = mysqli_query($db, $query);
+
+            if ($resultado->num_rows) {
+                $usuario = mysqli_fetch_assoc($resultado);
+                
+                //Revisar si password es correcto
+                $auth = password_verify($password, $usuario['password']);
+
+                if ($auth) {
+                    //El usuario está autenticado
+                    session_start();
+
+                    //Llenar arreglo de $_SESSION
+                    $_SESSION['usuario'] = $usuario['email'];
+                    $_SESSION['login'] = true;
+
+                    header('Location: ../../admin/site/index.php');
+
+                } else {
+                    $errores[] = "La contraseña es incorrecta";
+                }
+                
+            } else {
+                $errores[] = "El usuario no existe";
+            }
+        }
+    }
+
+    //Incluir header
     require '../../includes/functions.php';
     incluirTemplate('header');
     
 ?>
 
-<main class="contenedor seccion">
+<main class="contenedor seccion contenido-centrado">
     <h1>Iniciar sesión</h1>
 
-    <form action="" class="formulario">
+    <?php foreach($errores as $error) { ?>
+        <div class="alerta error">
+            <?= $error; ?>
+        </div>
+    <?php } ?>
+
+    <form method="POST" action="" class="formulario">
         <fieldset>
             <legend>Email y Password</legend>
-            <label for="nombre">Nombre</label>
-            <input type="text" id="nombre" placeholder="Tu nombre...">
 
             <label for="email">E-mail</label>
-            <input type="email" id="email" placeholder="Tu E-mail...">
+            <input type="email" name="email" id="email" placeholder="Tu E-mail...">
 
-            <label for="telefono">Teléfono</label>
-            <input type="tel" id="telefono" placeholder="Tu Teléfono...">
-
-            <label for="mensaje">Mensaje</label>
-            <textarea id="mensaje"></textarea>
+            <label for="password">Contraseña</label>
+            <input type="password" name="password" id="password" placeholder="Tu Contraseña...">
         </fieldset>
+
+        <input type="submit" value="Iniciar sesión" class="boton boton-verde">
     </form>
 </main>
 
