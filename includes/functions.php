b678377d6a9e981dc56037f739582afff4f66654
YailCastillo <yailcastillomtz@gmail.com>
2024-05-15 12:32:41 -0600

    estaAutenticado added

--- includes/functions.php
+++ includes/functions.php
@@ -6,3 +6,15 @@ function incluirTemplate($nombre, $inicio = false)
 {
     include TEMPLATES_URL . "/$nombre.php";
 }
+
+function estaAutenticado() : bool {
+    session_start();
+
+    $auth = $_SESSION['login'];
+
+    if ($auth) {
+        return true;
+    }
+
+    return false;
+}
