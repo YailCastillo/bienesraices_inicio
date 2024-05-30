017928f70f9435fd01f49a8675820bad3c1d4f84
YailCastillo <yailcastillomtz@gmail.com>
2024-05-30 13:54:43 -0600

    Created

---
+++ classes/Propiedad.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+class Propiedad {
+    public $id;
+    public $titulo;
+    public $precio;
+    public $imagen;
+    public $descripcion;
+    public $habitaciones;
+    public $wc;
+    public $estacionamiento;
+    public $creado;
+    public $vendedorId;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->precio = $args['precio'] ?? '';
+        $this->imagen = $args['imagen'] ?? 'imagen.jpg';
+        $this->descripcion = $args['descripcion'] ?? '';
+        $this->habitaciones = $args['habitaciones'] ?? '';
+        $this->wc = $args['wc'] ?? '';
+        $this->estacionamiento = $args['estacionamiento'] ?? '';
+        $this->creado = date('Y/m/d');
+        $this->vendedorId = $args['vendedorId'] ?? '';
+    }
+
+    public function save() {
+        //Insertar en la base de datos
+        $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) VALUES('$this->titulo', '$this->precio', '$this->imagen', '$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedorId')";
+
+        debugger($query);
+    }
+}
